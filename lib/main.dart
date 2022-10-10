f1b223be55fbabe28b9a848bc613dd20aa2bd11b
akshay-git20 <abpotkhule_b21@it.vjti.ac.in>
2022-10-10 20:24:17 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("welcome dunia"),
+          ),
+        ),
+      ),
+    );
+  }
+}
