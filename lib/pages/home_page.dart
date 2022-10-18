7e8634e07cb39aa96bfb1e9573bf7f8489de8d44
akshay-git20 <abpotkhule_b21@it.vjti.ac.in>
2022-10-18 21:46:24 +0530

    day 17 velocity x completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,8 +5,9 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_application_1/models/catelog.dart';
 import 'package:flutter_application_1/widgets/Item_Widegt.dart';
-
+import 'package:flutter_application_1/widgets/themes.dart';
 import '../widgets/drawer.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -38,45 +39,109 @@ class _HomePageState extends State<HomePage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
+      body: SafeArea(
+        child: Container(
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CatalogHeader(),
+                if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                  CatalogList().expand()
+                else
+                  Center(
+                    child: CircularProgressIndicator(),
+                  )
+              ],
+            )),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? GridView.builder(
-                itemCount: CatalogModel.items.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    mainAxisSpacing: 16.0,
-                    crossAxisSpacing: 16.0,
-                    crossAxisCount: 2),
-                itemBuilder: ((context, index) {
-                  final item = CatalogModel.items[index];
-                  return Card(
-                      child: GridTile(
-                    header: Container(
-                        decoration: BoxDecoration(color: Colors.deepPurple),
-                        child: Text(
-                          item.name,
-                          style: TextStyle(color: Colors.white),
-                        )),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Image.network(item.image),
-                    ),
-                    footer: Container(
-                        decoration: BoxDecoration(color: Colors.deepPurple),
-                        child: Text(
-                          item.price.toString(),
-                          style: TextStyle(color: Colors.white),
-                        )),
-                  ));
-                }))
-            : Center(
-                child: CircularProgressIndicator(),
-              ),
-      ),
-      drawer: MyDrawer(),
     );
+  }
+}
+
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(Mythemes.darkBluishColor).make(),
+        "Trending products".text.xl2.make()
+      ],
+    );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: CatalogModel.items.length,
+        itemBuilder: ((context, index) {
+          final item = CatalogModel.items[index];
+          return CatalogItem(item: item);
+        }));
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item item;
+  const CatalogItem({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(image: item.image),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              item.name.text.bold.color(Mythemes.darkBluishColor).make(),
+              item.desc.text.textStyle(context.captionStyle).make(),
+              ButtonBar(
+                buttonPadding: EdgeInsets.zero,
+                alignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  "\$${item.price}".text.bold.xl.make(),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all(StadiumBorder()),
+                          backgroundColor: MaterialStateProperty.all(
+                              Mythemes.darkBluishColor)),
+                      onPressed: (() {}),
+                      child: "Buy".text.bold.make())
+                ],
+              ).pOnly(right: 8)
+            ],
+          ),
+        )
+      ],
+    )).white.rounded.square(140).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+
+  const CatalogImage({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .rounded
+        .color(Mythemes.creamcolor)
+        .p8
+        .make()
+        .p16()
+        .w40(context);
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,12 +4,15 @@ import 'package:google_fonts/google_fonts.dart';
 class Mythemes {
   static ThemeData get lightTheme => ThemeData(
       appBarTheme: AppBarTheme(
-          color: Colors.deepPurple,
+          color: Mythemes.darkBluishColor,
           elevation: 0.0,
           titleTextStyle: TextStyle(color: Colors.black, fontSize: 22),
           iconTheme: IconThemeData(color: Colors.black)),
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily);
+      fontFamily: GoogleFonts.poppins().fontFamily);
+
+  static Color creamcolor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 
   static ThemeData get darkTheme => ThemeData(brightness: Brightness.dark);
 }
